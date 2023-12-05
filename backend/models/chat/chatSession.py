0f64a45131f05dbaec85d0e98891bb6f5a320ce9
MikeRock51 <mikerockmusic51@gmail.com>
2023-12-05 13:06:08 +0100

    Add Dummy data for frontend development

---
+++ backend/models/chat/chatSession.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""The chat model"""
+
+from sqlalchemy import Column, String, ForeignKey, Text
+from models.base_model import BaseModel, Base
+
+class Chat(BaseModel, Base):
+    """Defines a chat object"""
+
+    __tablename__ = "chats"
+
+    userID = Column(String(60), ForeignKey('users.id'), nullable=False)
+    content = Column(Text, nullable=False)
+    role = Column(String(100), nullable=False)

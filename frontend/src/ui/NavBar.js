851ef7c1aad21310c05dd35ac7916994f2f59b3c
Agareovo1 <ogheneovoagare@gmail.com>
2023-12-08 18:40:44 +0100

    updated claases

--- frontend/src/ui/NavBar.js
+++ frontend/src/ui/NavBar.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
-          const Links =[
-            {name: "HOME"},
-            {name: "CHATBOT"},
-            {name: "USERS"},
-            {name: "LOGOUT"},
-
-          ]
+function NavBar() {
   return (
     <div>
-       <nav className="bg-red-700 p-4 shadow-md h-16"> 
-        <ul>
-          {
-            Links.map((link,index)=>{
-              <li key={index} className='md: inline-block md:ml-10 ml-5'>
-                <a href='/' className='text-white text-sm'>{Link.name}</a>
-                
-              </li>
-            })
-          }
-        </ul>
-    </nav>
+      <nav className="bg-red-700 p-4 shadow-md md:flex md:justify-between md:items-center h-16">
+        <div className="container mx-auto flex justify-between items-center">
+          <ul className="flex space-x-4 md:space-x-8">
+            <li className="mr-6">
+              <Link to="/" className="text-white font-bold">Home</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/chat" className="text-white font-bold">ChatBot</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/users" className="text-white font-bold">Users</Link>
+            </li>
+          </ul>
+          <ul className="flex">
+            <li>
+              <Link to="/logout" className="text-white ml-auto font-bold">Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;

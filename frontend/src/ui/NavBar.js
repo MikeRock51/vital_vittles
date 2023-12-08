c9bf96a75c4c86730cd4d16bfea68e4c19cf7ad0
Agareovo1 <ogheneovoagare@gmail.com>
2023-12-08 18:18:09 +0100

    git push

--- frontend/src/ui/NavBar.js
+++ frontend/src/ui/NavBar.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function NavBar() {
+const NavBar = () => {
+          const Links =[
+            {name: "HOME"},
+            {name: "CHATBOT"},
+            {name: "USERS"},
+            {name: "LOGOUT"},
+
+          ]
   return (
     <div>
-       <nav className="bg-red-700 p-4 shadow-md"> 
-      <div className="container mx-auto flex justify-between items-center">
-        <ul className="flex space-x-72">
-          <li className="mr-6">
-            <Link to="/" className="text-white font-bold">Home</Link>
-          </li>
-          <li className="mr-6">
-            <Link to="/chat" className="text-white font-bold">ChatBot</Link>
-          </li>
-          <li className="mr-6">
-            <Link to="/users" className="text-white font-bold">Users</Link>
-          </li>
+       <nav className="bg-red-700 p-4 shadow-md h-16"> 
+        <ul>
+          {
+            Links.map((link,index)=>{
+              <li key={index} className='md: inline-block md:ml-10 ml-5'>
+                <a href='/' className='text-white text-sm'>{Link.name}</a>
+                
+              </li>
+            })
+          }
         </ul>
-        <ul className="flex">
-          <li>
-            <Link to="/logout" className="text-white ml-auto font-bold">Logout</Link>
-          </li>
-        </ul>
-      </div>
     </nav>
     </div>
   )

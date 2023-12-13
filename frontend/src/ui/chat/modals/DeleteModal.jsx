0f162c5b922afc2e8391c2b2d0281e921865deb1
MikeRock51 <mikerockmusic51@gmail.com>
2023-12-13 12:25:50 +0100

    Fix delete modal mobile interface

--- frontend/src/ui/chat/modals/DeleteModal.jsx
+++ frontend/src/ui/chat/modals/DeleteModal.jsx
@@ -20,9 +20,9 @@ function DeleteModal({ deleting, setDeleting }) {
       <div className="bg-primary-400 rounded-xl flex flex-col py-5">
         <h2 className="text-xl md:text-2xl text-white mx-auto mb-4">Delete Chat Topic?</h2>
         <hr className="border-[red]"/>
-        <p className="mx-auto mt-4 text-yellow-200">Are you sure you want to delete this chat?</p>
+        <p className="max-w-4/5 mx-auto px-5 mt-4 text-yellow-200">Are you sure you want to delete this chat?</p>
         <div className="flex flex-col xs:grid xs:grid-cols-2 mt-6 ">
-            <button className="xs:block w-3/5 mx-auto xs:ms-auto xs:me-1 py-1 bg-black hover:bg-gray-800 text-primary-400 rounded-lg" onClick={() => setDeleting(false)}>Close</button>
+            <button className="xs:block w-3/5 mx-auto xs:ms-auto xs:me-1 py-1 bg-black hover:bg-gray-800 text-primary-400 rounded-lg mb-2 xs:mb-0" onClick={() => setDeleting(false)}>Close</button>
             <button className="w-3/5 mx-auto xs:me-auto xs:ms-1 py-1 bg-[red] hover:bg-red-500 text-white rounded-lg" onClick={handleDelete}>Delete</button>
         </div>
       </div>

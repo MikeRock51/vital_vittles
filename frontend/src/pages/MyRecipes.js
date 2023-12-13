9dc7d811fd21a3e08e03a4cf5dae8373c213f814
George Iheanyi <georgeiheanyi@gmail.com>
2023-12-13 18:20:52 +0100

    Update

--- frontend/src/pages/MyRecipes.js
+++ frontend/src/pages/MyRecipes.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import Toast from "../providers/ToastProvider";
+import toast from "react-hot-toast";
 
-function MyRecipes() {
+import CardItem from "../components/CardItem";
+// import { useEffect, useState } from "react";
+import Loader from "../ui/Loader";
+import axios from "axios";
+// import { useRecipesContext } from "../context/RecipesContext";
+import SearchRecipe from "../components/SearchRecipe";
+import { useRecipeStore } from "../stateProvider/recipeStore";
+import { useEffect } from "react";
+// toast.success("Toast setup successfully!");
+
+const API_URL = "https://acr-api.mikerock.tech/api/v1/recipes/me";
+const PAGE_SIZE = 10;
+
+export default function MyRecipes() {
+  // const [recipes, setRecipes] = useState({ data: [] });
+  // const [currentPage, setCurrentPage] = useState(1);
+  const {
+    recipes,
+    setRecipes,
+    currentPage,
+    setCurrentPage,
+    searchTerm,
+    setSearchTerm,
+  } = useRecipeStore();
+  // const [fetch, setFetch] = useState(false);
+
+  // const { recipes, currentPage, dispatch } = useRecipesContext();
+
+  const handleLoadMore = () => {
+    // dispatch({ type: "NEXT_PAGE", payload: currentPage + 1 });
+    setCurrentPage(currentPage + 1);
+  };
+
+  // console.log(searchTerm);
+  const fetchData = async () => {
+    setSearchTerm("");
+    try {
+      const response = await axios.get(
+        `${API_URL}?page=${currentPage}&pageSize=${PAGE_SIZE}${
+          searchTerm ? `&search=${searchTerm}` : ""
+        })}`,
+      );
+      const newData = response?.data?.data;
+      console.log(newData);
+      setCurrentPage(Number(response?.data?.page));
+
+      // dispatch({ type: "GET_RECIPES", payload: newData });
+      setRecipes([...recipes, ...newData]);
+      // setRecipes((prevData) => ({
+      //   data: [...prevData.data, ...newData.data],
+      // }));
+    } catch (error) {
+      console.log("Error fetching recipes", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [currentPage]);
   return (
-    <div>MyRecipes</div>
-  )
-}
+    <div className="mt-20">
+      <Toast />
 
-export default MyRecipes
+      <SearchRecipe />
+
+      <h1 className="mb-4 text-center text-3xl font-bold">My Recipes</h1>
+
+      {recipes ? (
+        <div className="flex flex-col items-center">
+          <ul className="flex flex-wrap items-center justify-center gap-20 ">
+            {recipes?.map((recipe) => (
+              <CardItem key={recipe.id} id={recipe.id} name={recipe.name} />
+            ))}
+          </ul>
+
+          {recipes.length === 0 ? (
+            ""
+          ) : (
+            <button
+              className="mx-auto mt-6 rounded-lg bg-orange-300 px-7 py-2.5 text-center text-sm font-bold text-stone-800 outline-none drop-shadow-xl transition-colors duration-300 hover:bg-orange-400 focus:outline-none focus:ring focus:ring-orange-400 focus:ring-offset-2 "
+              onClick={handleLoadMore}
+            >
+              Load More...
+            </button>
+          )}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+}
